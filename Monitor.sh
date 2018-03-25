86019392af02d5a0a07b70e393cef4a60195a917
endurablepants <endurablepants@hotmail.com>
2018-03-25 11:27:19 -0700

    Iot-hub monitoring

---
+++ Monitor.sh
@@ -0,0 +1,1 @@
+./iothub-explorer.js monitor-events AndrewsPi --login "HostName=PiClouds.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=K3PDPENckRvV8vzXpxDaEZM+kwKelAGT5mwsYqYVlyU="
